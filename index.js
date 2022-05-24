96b522cff016cccf926f6bc988bbba0192289bca
rakibul114 <rakibulislam114@gmail.com>
2022-05-24 09:47:11 +0600

    client side connected to database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,26 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express());
+
+
+// MongoDB connection
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6rxra.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+    const collection = client.db("test").collection("devices");
+    console.log('Manufacturer connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 
 
 // Root API
