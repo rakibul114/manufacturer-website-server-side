82f1e052e335c8767bbccdf926106af0fb057ffd
rakibul114 <rakibulislam114@gmail.com>
2022-05-26 09:38:15 +0600

    created a funtion to verify jwt token

--- index.js
+++ index.js
@@ -21,27 +21,83 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// verify jwt token
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run() {
     try {
-        await client.connect();
-        const toolCollection = client.db("toolsManufacturer").collection("tool");
-        
-        // Get all tools data
-        app.get('/tool', async (req, res) => {
-            const query = {};
-            const cursor = toolCollection.find(query);
-            const tools = await cursor.toArray();
-            res.send(tools);
-        });
+      await client.connect();
+      const toolCollection = client.db("toolsManufacturer").collection("tool");
+      const orderCollection = client
+        .db("toolsManufacturer")
+        .collection("order");
+      const userCollection = client.db("toolsManufacturer").collection("users");
 
-
-        // Get single tool data
-        app.get('/tool/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const tool = await toolCollection.findOne(query);
-            res.send(tool);
+      // Verify Admin
+      const verifyAdmin = async (req, res, next) => {
+        const requester = req.decoded.email;
+        const requesterAccount = await userCollection.findOne({
+          email: requester,
         });
+        if (requesterAccount.role === "admin") {
+          next();
+        } else {
+          res.status(403).send({ message: "forbidden" });
+        }
+      };
+
+      // get all users from database
+      app.get("/user", verifyJWT, async (req, res) => {
+        const users = await userCollection.find().toArray();
+        res.send(users);
+      });
+
+      // get admin email
+      app.get("/admin/:email", async (req, res) => {
+        const email = req.params.email;
+        const user = await userCollection.findOne({ email: email });
+        const isAdmin = user.role === "admin";
+        res.send({ admin: isAdmin });
+      });
+      
+
+      // Get all tools data
+      app.get("/tool", async (req, res) => {
+        const query = {};
+        const cursor = toolCollection.find(query);
+        const tools = await cursor.toArray();
+        res.send(tools);
+      });
+
+      // Get single tool data
+      app.get("/tool/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const tool = await toolCollection.findOne(query);
+        res.send(tool);
+      });
+
+      // Order Collection API
+      app.post("/order", async (req, res) => {
+        const order = req.body;
+        console.log(order);
+        const result = await orderCollection.insertOne(order);
+        res.send(result);
+      });
     }
     finally {
         
@@ -55,7 +111,7 @@ run().catch(console.dir);
 // Root API
 app.get('/', (req, res) => {
     res.send('Running Manufacturer Server');
-})
+});
 
 // APP listener
 app.listen(port, () => {
